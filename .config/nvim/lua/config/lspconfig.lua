9040e7bdacf3549e65d60271a438eea0004b63ac
lutheran736 <sweetmask20@murena.io>
2024-10-27 20:04:58 -0400

    remote  server_settings

--- .config/nvim/lua/config/lspconfig.lua
+++ .config/nvim/lua/config/lspconfig.lua
@@ -75,22 +75,4 @@ M.defaults = function()
   }
 end
 
-local server_settings = {
-  ruff_lsp = {},
-  tsserver = {},
-  pyright = {
-    pyright = {
-      -- Using Ruff's import organizer
-      disableOrganizeImports = true,
-    },
-    python = {
-      analysis = {
-        -- Ignore all files for analysis to exclusively use Ruff for linting
-        ignore = { '*' },
-        autoImportCompletions = false,
-      },
-    },
-  }
-}
-
 return M
