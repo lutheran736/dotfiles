d8ad969eb0276397035cf6dfc0db6a1046067c91
lutheran736 <sweetmask20@murena.io>
2024-10-30 18:04:59 -0400

    minor changes

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -15,14 +15,20 @@ return {
     end,
   },
 
-  "nvim-lua/plenary.nvim",
-  "nvim-tree/nvim-web-devicons",
+  { "nvim-lua/plenary.nvim" },
+  { "nvim-tree/nvim-web-devicons" },
 
   {
     "lukas-reineke/indent-blankline.nvim",
     event = "User FilePost",
-    config = function()
-      require("ibl").setup()
+    opts = {
+      indent = { char = "|" },
+      scope = { char = "|" },
+    },
+    config = function(_, opts)
+      local hooks = require "ibl.hooks"
+      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+      require("ibl").setup(opts)
     end,
   },
 
